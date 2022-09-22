597fccce288dfdf2c5fe74a59863352c9cfee1ad
Victor Hugo <victorhrapinto@gmail.com>
2022-09-22 10:21:11 -0300

    atualizacao sobre

--- src/pages/sobre/index.js
+++ src/pages/sobre/index.js
@@ -14,13 +14,13 @@ export default function App(){
                         <a href="https://gitlab.com/projeto-multidisciplinar/heleno-app" target="_blank" rel="noreferrer">
                             <button className="btnImagem">
                                 <img src={fotoTurmaAntiga} className="imagem"/>
-                                Ver documentação antiga
+                                Documentação 
                             </button>
                         </a>
                         <a href="https://gitlab.com/EduardoMangeli/ibtech" target="_blank" rel="noreferrer">
                             <button className="btnImagem">
                                 <img src={fotoTurmaAntiga}  className="imagem"/>
-                                Ver documentação nova
+                                Nova Documentação
                             </button>
                         </a>     
                     </div>    
@@ -29,7 +29,7 @@ export default function App(){
                         <h2 className="TituloSobre">
                             <p>Criado inicialmente por Guilherme Félix, Igor Mariano, Felipe Castelhano e Clarissa Proença.</p>
 
-                            Assumido após por Victor Hugo Rocha, Amanda Senra, Matheus Herzog, William Fagundes, Caio da luz, Gabriel Espósito,<br/> Guilherme Maia, Ilidio , João Victor Meirelles, Leonardo Campello, Maria Giulia, Pedro Reis, Augusto Amorim, Thaís Bustamante, Gabriel Oscar, Vitor Lobianco, Raphael Meres, João Curvelho, Théo Furtado, Rafael Neiva
+                            Assumido após por Victor Hugo Rocha, Amanda Senra, Matheus Herzog, William Fagundes, Caio da luz, Gabriel Espósito,<br/> Guilherme Maia, Ilidio , João Victor Meirelles, João Pedro Constant, Leonardo Campello, Maria Giulia, Pedro Reis, Augusto Amorim, Thaís Bustamante, Gabriel Oscar, Vitor Lobianco, Raphael Meres, João Curvelho, Théo Furtado, Rafael Neiva, Beatriz Moura, Gabriel Martinez e João Pedro Correia.
                         </h2>
                         <h2 className="TituloSobre">
                             O Projeto ibtech originou-se como o projeto semestral dos alunos de Análise e Desenvolvimento de Sistemas do Ibmec-Barra, dirigido e direcionado pelo professor <i>Eduardo Mangeli</i>,
