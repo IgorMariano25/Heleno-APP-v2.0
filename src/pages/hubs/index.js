206f1d5a011837eebd825c04e16ea495da288b86
Giulia Pessanha <mgpessanh@gmail.com>
2022-09-27 16:24:24 -0300

    add texto

--- src/pages/hubs/index.js
+++ src/pages/hubs/index.js
@@ -6,11 +6,19 @@ import './styles.css';
 
 
 
+
 export default function App(){
   return(
     <div>
         <Topo/>
-          <img    id='logohubs'
+       
+        <h1>O que é o HUBs?</h1>           
+        <br></br>                   
+        <p>Um espaço colaborativo de inovação, presente nas quatro unidades Ibmec (BH, SP, RJ e Brasília),</p>
+        <p>que busca estimular o networking e o empreendedorismo por meio da conexão</p>
+        <p>entre empreendedores locais, mentores e empresas.</p>
+        
+        <img    id='logohubs'
                   src={hubs}
                   alt={'primeira imagem hubs'}
           />
