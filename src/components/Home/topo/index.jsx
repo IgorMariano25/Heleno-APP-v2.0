1d215e461fe80f65b348d7bb4d54565c6c30411e
theofurtado05 <theofurtado05@gmail.com>
2022-09-27 19:04:10 -0300

    Style e header inicial

--- src/components/Home/topo/index.jsx
+++ src/components/Home/topo/index.jsx
@@ -1,43 +1,26 @@
-import React from "react";
-import { Card, Img, Textos } from "./styles.js";
+import { Card, Img, Textos } from "./Style.js";
 import {Link} from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
-
-
-
 
 const Topo = ( props ) => {
     const navigate = useNavigate();    
     return(
         <nav>
-            <Card>
-                <Img
+        <Card>
+            <Img
                     src={ `${process.env.PUBLIC_URL}/imagens/logoIBMECFundoAzul.png` }
                     alt={'logo do IBMEC'}
                     onClick={()=>navigate("/")}
-                />
-                <Textos>
-                    <ul>
-                        <li><Link to="/portfolios">Portfólios</Link></li>
-                        <li><Link to="/alunos">Alunos</Link></li>
-                        <li><Link to="/contato">Contato</Link></li>
-                        <li><Link to="/sobre">Sobre</Link></li>
-                        <li id="li-educacao">Educação
-                            <ul id="menu-educacao">
-                                <li><Link to="/mba">MBA</Link></li>
-                                <li><Link to="/pos">Pós</Link></li>
-                                <li><Link to="/graduacao">Graduação</Link></li>
-                            </ul>
-                        </li>
-                    </ul>
-                    
-                    
-                </Textos>
-            </Card>
+            />
+            <Textos>
+                <Link to="/portfolios">Portfólios</Link>
+                <Link to="/alunos">Alunos</Link>
+                <Link to="/contato">Contato</Link>
+                <Link to="/sobre">Sobre</Link>
+            </Textos>
+        </Card>
         </nav>
     );
 }
-
-
 
 export default Topo;
--- src/components/Home/topo/styles.js
+++ src/components/Home/topo/styles.js
@@ -1,88 +1,108 @@
 import styled from "styled-components";
 
+
 const Card = styled.div`
+
     top: 0px;
+
     left: 2.083em;
+
     width: 100%;
+
     height: 4.6875em;
+
     background-color: #002555;
+
     display:flex;
+
     a {
+
         text-decoration: none;
+
         color: white;
-        
+
+        margin-right: 35px;
+
         transition: --offset 300ms, text-decoration-color 300ms;
+
     }
+
     a:hover{
+
         text-decoration: underline;
+
         text-underline-offset: 0.4em;
+
         color: #F5AC00
+
     }
+
 `;
+
 
 const Img = styled.img`
+
     height: 3em;
+
     margin-right: 0%;
+
     margin: 15px;
+
     margin-left: 10em;
+
 `;
+
 
 const Textos = styled.div`
-    font-family: 'Barlow';
-    font-weight: 500;
-    font-style: italic;
-    font-size: 18px;
-    flex-grow: 1;
-    padding: 0.8em;
-    border-radius: 0  8px  8px 0;
-    text-decoration: none;
-    color:white;
-    margin: 1%;
-    margin-left: 10px;
-    ul{
-        list-style: none;
-    }
-    ul li{
-        display: inline;
-        margin-right: 35px;
-    }
-    ul li ul{
-        width: 200px;
-        text-align: center;
-        
-        margin-left: 300px;
-    }
-    ul li ul li{
-        background-color: grey;
-        display: none;
-        text-align: center;
-        margin-right: 0px;
-        padding: 10px;
-        box-shadow: 0 0 3px 0 black;
-    }
 
-    #li-educacao:hover{
-        cursor: pointer;
-        color: #F5AC00
-    }
-    
-    #li-educacao:hover > #menu-educacao li{
-        display: block;
-    }
+    font-family: 'Barlow';
+
+    font-weight: 500;
+
+    font-style: italic;
+
+    font-size: 18px;
+
+    flex-grow: 1;
+
+    padding: 0.8em;
+
+    border-radius: 0  8px  8px 0;
+
+    text-decoration: none;
+
+    color:white;
+
+    margin: 1%;
+
+    margin-left: 10px;
+
 `;
+
 
 const DivRede = styled.div`
+
     margin-right: 230px;
+
 `;
+
 
 const ListaRede = styled.ul`
+
     display: flex;
+
     list-style-type: none;
+
     align-items: center;
+
 `;
 
+
 const IconeRedeSocial = styled.img`
+
     height: 18px;
+
 `;
+
 
 export { Card, Img, Textos, DivRede, ListaRede, IconeRedeSocial}
