0330d1b7c8475c08a735defcf89c397ef8c3fdc9
lobiancovitor <lobiancocnvitor@hotmail.com>
2022-09-28 00:00:17 -0300

    inclusão aba Hubs

--- src/pages/hubs/index.js
+++ src/pages/hubs/index.js
@@ -11,17 +11,38 @@ export default function App(){
   return(
     <div>
         <Topo/>
-       
-        <h1>O que é o HUBs?</h1>           
-        <br></br>                   
-        <p>Um espaço colaborativo de inovação, presente nas quatro unidades Ibmec (BH, SP, RJ e Brasília),</p>
-        <p>que busca estimular o networking e o empreendedorismo por meio da conexão</p>
-        <p>entre empreendedores locais, mentores e empresas.</p>
-        
         <img    id='logohubs'
                   src={hubs}
                   alt={'primeira imagem hubs'}
-          />
+          />  
+       
+        <h1 className="Título">O que é o HUBs?</h1>        
+        <br></br>                
+        <div className="Texto">
+            <p>Um espaço colaborativo de inovação, presente nas quatro unidades Ibmec (BH, SP, RJ e Brasília), que busca estimular o networking e o empreendedorismo por meio da conexão entre empreendedores locais, mentores e empresas.
+            </p>
+
+            <p>Fazendo parte do HUBs você pode:</p>
+            <ul>
+                <li>
+                    Contar com uma infraestrutura moderna e confortável dotada de espaço de coworking, salas de trabalho individuais e sala de reunião;
+                </li>
+                <li>
+                    Participar de mentoria com empreendedores e profissionais de mercado;
+                </li>
+                <li>
+                    Participar de palestras, minicursos e workshops com professores do Ibmec;
+                </li>
+                <li>
+                    Participar de desafios de negócios do Hubs e de empresas parceiras;
+                </li>
+                <li>
+                    Fazer networking.
+                </li>
+            </ul>
+            
+        </div>
+    
         <Footer/>
     </div>
   );
