0a5dbce2712a20123ef9de4589b681df7be56eec
William <ibmecfagundes@gmail.com>
2022-09-28 13:16:13 -0300

    Assinatura de codigo

--- src/components/topo/topo.jsx
+++ src/components/topo/topo.jsx
@@ -2,40 +2,44 @@ import { Card, Img, Textos } from "./Style.js";
 import { useNavigate } from "react-router-dom";
 import './ham.css';
 
+/*Created and developed exclusively by William Fagundes*/
+/*Created and developed exclusively by William Fagundes*/
+/*Created and developed exclusively by William Fagundes*/
 
 const Topo = ( props ) => {
     const navigate = useNavigate();    
     return(
         <nav>
-        
-        <Card>
-            <img    id="logoibmec"
-                    src={ `./imagens/logoIBMECFundoAzul.png` }
-                    alt={'logo do IBMEC'}
-                    onClick={()=>navigate("/")}
-            />
-            <div id="menuToggle">
-                <input type="checkbox" class="botao" onClick="change()" />
-                <span></span>
-                <span></span>
-                <span></span>
-                    
-                        <ul id="menu">
-                            <a href="/alunos"><li>Alunos</li></a>
-                            <a href="/contato"><li>Contato</li></a>
-                            <a href="/sobre"><li>Sobre</li></a>
-                            <a href="/portifolios"><li>Portifolios</li></a>
-                            <img id="logoibmecmenu"
-                                 src={ `./imagens/logoIBMECFundoAmarelo.png` }
-                                 alt={'logo do IBMEC'}
-                                 onClick={()=>navigate("/")}
-                                />
-                        </ul>
-                    
-            </div>
-        </Card>
+            <Card>
+                <img    id="logoibmec"
+                        src={ `./imagens/logoIBMECFundoAzul.png` }
+                        alt={'logo do IBMEC'}
+                        onClick={()=>navigate("/")}
+                />
+                <div id="menuToggle">
+                    <input type="checkbox" class="botao" onClick="change()" />
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <ul id="menu">
+                        <a href="/alunos"><li>Alunos</li></a>
+                        <a href="/contato"><li>Contato</li></a>
+                        <a href="/sobre"><li>Sobre</li></a>
+                        <a href="/portifolios"><li>Portifolios</li></a>
+                        <img id="logoibmecmenu"
+                            src={ `./imagens/logoIBMECFundoAmarelo.png` }
+                            alt={'logo do IBMEC'}
+                            onClick={()=>navigate("/")}
+                        />
+                    </ul>   
+                </div>
+            </Card>
         </nav>
     );
 }
+
+/*Created and developed exclusively by William Fagundes*/
+/*Created and developed exclusively by William Fagundes*/
+/*Created and developed exclusively by William Fagundes*/
 
 export default Topo;
