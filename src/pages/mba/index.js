bd91af34bc9c35fd85fe7530e390e3c6e250c5e7
Theo Furtado <theofurtado05@gmail.com>
2022-11-03 08:08:12 -0300

    Resolve ai William

--- src/pages/mba/index.js
+++ src/pages/mba/index.js
@@ -1,8 +1,33 @@
+import Master from '../masterPage'
+import banner from './assets/bannerMBA.png';
+import './style.css';
+
+
 export default function MBA(){
     return(
-        <div>
-            Online
-        </div>
+        <Master>
+            <Header/>
+            
+            <div className="banner">
+                <img
+                    id='bannerMBA'
+                    src={banner}
+                    alt={'MBA Ibmec'}
+                />
+            </div>
+            
+            <div className="diferenciais">
+                
+
+
+            </div>
+
+            <div className="cursos">
+
+
+            </div>
+
+            <Footer/>
+        </Master>
     )
 }
-{/*IMPORTAR TOPO E FOOTER*/}
